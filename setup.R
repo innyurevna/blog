cc714f153ab4970a3bc7154e3c41b33efa05fbe1
innyurevna <innyurevna@users.github.com>
2020-03-10 23:03:00 +0300

    Commented a line

--- setup.R
+++ setup.R
@@ -1,6 +1,6 @@
 library(blogdown)
 new_site(theme = "gcushen/hugo-academic", theme_example = TRUE)
 serve_site()
-install_theme("gcushen/hugo-academic", theme_example = TRUE, update_config = TRUE)
+# install_theme("gcushen/hugo-academic", theme_example = TRUE, update_config = TRUE)
 servr::daemon_stop(1)
 install_theme("bjacquemet/personal-web", theme_example = TRUE, update_config = TRUE)
